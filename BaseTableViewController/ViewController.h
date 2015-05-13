8f814075b049571cee639f4a2b416315c2cad624
zhenglinqin <zheng.linqin@hongfu001.com>
2015-05-13 17:32:29 +0800

    Initial Commit

---
+++ BaseTableViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaseTableViewController
+//
+//  Created by Zhenglinqin on 15/5/13.
+//  Copyright (c) 2015年 Binglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BaseTableViewController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BaseTableViewController
+//
+//  Created by Zhenglinqin on 15/5/13.
+//  Copyright (c) 2015年 Binglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
